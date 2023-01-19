1f50ad5aa15624b4c32617d5334c5b5178c43b06
Hanane-fallah <hananee.fallah@gmail.com>
2023-01-20 01:28:10 +0330

    004 - add category filtered list view

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('all_tasks/', views.TaskList.as_view(), name='task_list'),
+    path('all_tasks/<slug:slug>/', views.CatList.as_view(), name='task_list'),
+
     path('task_detail/<slug:slug>', views.TaskDetail.as_view(), name='task_detail'),
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -10,9 +10,21 @@ class TaskList(ListView):
     paginate_by = 5
     ordering = ['due_date']
 
+
 class CatList(ListView):
     model = Task
     paginate_by = 5
     ordering = ['due_date']
+
+    def get_queryset(self):
+        return Task.objects.filter(category=self.kwargs.get('slug'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['slug'] = self.kwargs.get('slug')
+        return context
+    # queryset =
+
+
 class TaskDetail(DetailView):
     model = Task
